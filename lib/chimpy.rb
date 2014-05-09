eb6434f9c1fc65c2c8a938c9e1c44cbdfcf79835
Ignacio Gutierrez <nachojgutierrez@gmail.com>
2014-05-09 16:39:32 -0300

    chimpy brings users who need to be synced

--- lib/chimpy.rb
+++ lib/chimpy.rb
@@ -2,8 +2,10 @@ require "chimpy/version"
 
 module Chimpy
   class Base
-    def self.hello
-      "Hello"
+    def self.users_to_sync
+      never_synced = User.where(chimpy_synced_at: nil)
+      needing_sync = User.where('updated_at > chimpy_synced_at')
+      never_synced + needing_sync
     end
   end
 end
